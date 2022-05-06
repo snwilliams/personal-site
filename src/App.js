861512eb14e1afb8262fbca7c109df245cdea199
Sara Williams <sara.williams.n@gmail.com>
2022-05-06 11:41:40 -0400

    added basic css and intro text

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Sara Williams</h1>
+      <h1 className="intro-text">Hi, I'm Sara.</h1>
+      <p>I created this website using ReactJS to show that I'm more than just a few bullets on a resume. What I lack in programming experience, I
+        make up for in determination to learn and great communication and teamwork skills. I have a lot to offer the right team, so if you're 
+        searching for a dedicated employee, let's talk!
+      </p>
+      <a href="mailto:sara.williams.n@gmail.com"><button className="contact-me">Contact Me</button></a>
     </div>
   );
 }

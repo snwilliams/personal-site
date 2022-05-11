072adc4863bc8a8ea8a3cee4b91957306e4b67f8
Sara Williams <sara.williams.n@gmail.com>
2022-05-11 14:38:43 -0400

    added Router to project and started building Nav page

---
+++ src/pages.js
@@ -0,0 +1,28 @@
+export function Home() {
+    return(
+        <div>
+            <h1 className="intro-text">Hi, I'm Sara.</h1>
+            <p>I created this website using ReactJS to show that I'm more than just a few bullets on a resume. What I lack in programming experience, I
+            make up for in determination to learn and great communication and teamwork skills. I have a lot to offer the right team, so if you're 
+            searching for a dedicated employee, let's talk!
+            </p>
+            <a href="mailto:sara.williams.n@gmail.com"><button className="contact-me">Contact Me</button></a>
+        </div>
+    );
+}
+
+export function Resume() {
+    return (
+        <div>
+            <h1>My Resume</h1>
+        </div>
+    );
+}
+
+export function MyProjects() {
+    return (
+        <div>
+            <h1>My Projects</h1>
+        </div>
+    );
+}

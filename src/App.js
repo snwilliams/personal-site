bc4176b2f9a9ad7c2922d6299f91bdd890e2c509
Sara Williams <sara.williams.n@gmail.com>
2022-05-11 14:52:18 -0400

    installed boostrap and used boostrap to make a nav bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
+import Nav from './Nav';
 import { Routes, Route } from 'react-router';
 import { Home, Resume, MyProjects } from "./pages";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/resume" element={<Resume />} />
-      <Route path="/projects" element={<MyProjects />} />
-    </Routes>
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/projects" element={<MyProjects />} />
+      </Routes>
+    </>
   );
 }
 

b46e3b4ca592444a7d14052c89ae35743daa8988
Sara Williams <sara.williams.n@gmail.com>
2022-05-12 15:40:27 -0400

    added sticky footer to site with contact information

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer () {
+    return (
+        <footer className="footer">
+            <p className="footer-text">&copy; Sara Williams 2022  | sara.williams.n@gmail.com  | (970) 412-4699 </p>
+        </footer>
+    )
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Nav from './Nav';
 import App from './App';
+import Footer from './Footer';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
@@ -11,6 +12,7 @@ root.render(
     <Router>
         <div className="nav-bar"><Nav /></div>
         <div className="main-content"><App /></div>
+        <Footer />
     </Router>
 );
 

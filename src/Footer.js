edd59c8e608eba924ceebbb941ca518607b384bb
Sara Williams <sara.williams.n@gmail.com>
2022-05-12 15:43:53 -0400

    added mailto link to email in footer and styling

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer () {
     return (
         <footer className="footer">
-            <p className="footer-text">&copy; Sara Williams 2022  | sara.williams.n@gmail.com  | (970) 412-4699 </p>
+            <p className="footer-text">&copy; Sara Williams 2022  | <a className="footer-email" href="mailto:sara.williams.n@gmail.com">sara.williams.n@gmail.com</a>  | (970) 412-4699 </p>
         </footer>
     )
 }

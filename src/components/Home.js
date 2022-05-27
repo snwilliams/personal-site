5d0086eb1f29c387341455db17f69566d0d006e1
Sara Williams <sara.williams.n@gmail.com>
2022-05-27 15:26:36 -0400

    change intro text to be less lame

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,9 @@ export function Home() {
         <div className="home">
             <img className="profile-photo" src={sara} alt="Sara Williams, a white woman with brown hair" />
             <h1 className="intro-text">Hi, I'm Sara.</h1>
-            <p className="intro-p">I created this website using ReactJS to show that I'm more than just a few bullets on a resume. What I lack in programming experience, I
-            make up for in determination to learn and great communication and teamwork skills. I have a lot to offer the right team, so if you're 
-            searching for a dedicated employee, let's talk!
+            <p className="intro-p">
+                I'm a U.S. based software developer at the start of my career, currently looking for a new opportunity with a mission-driven organization that values developer growth.
+                Check out my Projects and my Resume to learn more about my experience and what I've been working on, and if I could be a good fit for your organization, let's talk!
             </p>
             <a href="mailto:sara.williams.n@gmail.com"><button className="contact-me">Contact Me</button></a>
         </div>
